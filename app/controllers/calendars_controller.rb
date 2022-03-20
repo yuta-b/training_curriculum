452a32130ed0d71061a27587b11615d1a465cabe
yuta-b <yuta1080279@gmail.com>
2022-03-20 14:50:23 +0900

    Issue4~6の再修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,7 +37,7 @@ class CalendarsController < ApplicationController
 
       wday_num = Date.today.wday
       wday_num += 1
-      if wday >= 7
+      if wday_num >= 7
         wday_num = wday_num -7
       end
 
